73f939bf2f480c5dac0b30b7db5300561a627b80
thiago-gouveia <thiagogh1@al.insper.edu.br>
2025-03-31 11:01:29 -0300

    feito

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,40 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int BTN_PIN_G = 26;
+const int BTN_PIN_R = 28;
+const int LED_PIN_R = 4;
+const int LED_PIN_G = 6;
+
 int main() {
   stdio_init_all();
 
+  gpio_init(BTN_PIN_G);
+  gpio_set_dir(BTN_PIN_G, GPIO_IN);
+  gpio_pull_up(BTN_PIN_G);
+
+  gpio_init(BTN_PIN_R);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+  gpio_pull_up(BTN_PIN_R);
+
+  gpio_init(LED_PIN_R);
+  gpio_set_dir(LED_PIN_R, GPIO_OUT);
+  gpio_init(LED_PIN_G);
+  gpio_set_dir(LED_PIN_G, GPIO_OUT);
+
+  bool led_state_1 = false;
+  bool led_state_2 = false;
+
   while (true) {
+    if (!gpio_get(BTN_PIN_G)) {
+      led_state_1 = !led_state_1;
+      gpio_put(LED_PIN_G, led_state_1);
+      sleep_ms(250);
+    }
+    if (!gpio_get(BTN_PIN_R)) {
+      led_state_2 = !led_state_2;
+      gpio_put(LED_PIN_R, led_state_2);
+      sleep_ms(250);
+    }
   }
 }
--- exe5/main.c
+++ exe5/main.c
@@ -2,30 +2,30 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
-const int BTN_PIN = 26;
-const int BTN_PIN_2 = 7;
+const int BTN_PIN_G = 26;
+const int BTN_PIN_B = 7;
 
 int main() {
     stdio_init_all();
 
-    gpio_init(BTN_PIN);
-    gpio_set_dir(BTN_PIN, GPIO_IN);
-    gpio_pull_up(BTN_PIN);
+    gpio_init(BTN_PIN_G);
+    gpio_set_dir(BTN_PIN_G, GPIO_IN);
+    gpio_pull_up(BTN_PIN_G);
 
-    gpio_init(BTN_PIN_2);
-    gpio_set_dir(BTN_PIN_2, GPIO_IN);
-    gpio_pull_up(BTN_PIN_2);
+    gpio_init(BTN_PIN_B);
+    gpio_set_dir(BTN_PIN_B, GPIO_IN);
+    gpio_pull_up(BTN_PIN_B);
     int cnt_1 = 0;
     int cnt_2 = 0;
 
     while (true) {
 
-        if (!gpio_get(BTN_PIN)) {
-            printf("Botao 1: %d\n", cnt_1++);
+        if (!gpio_get(BTN_PIN_G)) {
+            printf("Botao G: %d\n", cnt_1++);
         }
 
-        if (!gpio_get(BTN_PIN_2)) {
-            printf("Botao 2: %d\n", cnt_2++);
+        if (!gpio_get(BTN_PIN_B)) {
+            printf("Botao B: %d\n", cnt_2++);
         }
     }
 }
